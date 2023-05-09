b207be21da06ef8a7206623d0dbe1250a7f3d702
iqbaliamal <iqbaliamal@gmail.com>
2023-05-09 15:54:50 +0700

    fix: route group and add other dependencies for unit test

--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.19
 require github.com/golang-jwt/jwt v3.2.2+incompatible
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -27,6 +33,7 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
+	github.com/stretchr/testify v1.8.2
 	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.45.0 // indirect
--- route/route.go
+++ route/route.go
@@ -11,30 +11,30 @@ import (
 
 func RouteInit(r *fiber.App) {
 
-	r.Group("/api/v1")
+	route := r.Group("/api/v1")
 
-	r.Static("/public", config.ProjectRootPath+"/public") // untuk alternative path jikalau path dibawah tidak terbaca
-	r.Static("/public", "./public")                       // gunakan ini terlebih dahulu
+	route.Static("/public", config.ProjectRootPath+"/public") // untuk alternative path jikalau path dibawah tidak terbaca
+	route.Static("/public", "./public")                       // gunakan ini terlebih dahulu
 
-	r.Post("/login", controllers.Login)
+	route.Post("/login", controllers.Login)
 
-	r.Get("/user", middleware.Auth, controllers.UserGetAll)
-	r.Get("/user/me", middleware.Auth, controllers.UserMe)
-	r.Post("/user", controllers.UserCreate)
-	r.Put("/user", middleware.Auth, controllers.UserUpdate)
-	r.Put("/user/update-email", middleware.Auth, controllers.UserUpdateEmail)
+	route.Get("/user", middleware.Auth, controllers.UserGetAll)
+	route.Get("/user/me", middleware.Auth, controllers.UserMe)
+	route.Post("/user", controllers.UserCreate)
+	route.Put("/user", middleware.Auth, controllers.UserUpdate)
+	route.Put("/user/update-email", middleware.Auth, controllers.UserUpdateEmail)
 
-	r.Get("/scholarship", controllers.ScholarshipGetAll)
-	r.Post("/scholarship", middleware.Auth, utils.HandleSingleFile, controllers.ScholarshipCreate)
-	r.Put("/scholarship/:id", middleware.Auth, controllers.ScholarshipUpdate)
-	r.Delete("/scholarship/:id", middleware.Auth, controllers.ScholarshipDelete)
-	r.Get("/scholarship/:id", controllers.ScholarshipGetById)
+	route.Get("/scholarship", controllers.ScholarshipGetAll)
+	route.Post("/scholarship", middleware.Auth, utils.HandleSingleFile, controllers.ScholarshipCreate)
+	route.Put("/scholarship/:id", middleware.Auth, controllers.ScholarshipUpdate)
+	route.Delete("/scholarship/:id", middleware.Auth, controllers.ScholarshipDelete)
+	route.Get("/scholarship/:id", controllers.ScholarshipGetById)
 
-	r.Get("/category", controllers.CategoryGetAll)
-	r.Get("/category/:id", controllers.CategoryGetByID)
-	r.Post("/category", middleware.Auth, controllers.CategoryCreate)
-	r.Put("/category/:id", middleware.Auth, controllers.CategoryUpdate)
-	r.Delete("/category/:id", middleware.Auth, controllers.CategoryDelete)
+	route.Get("/category", controllers.CategoryGetAll)
+	route.Get("/category/:id", controllers.CategoryGetByID)
+	route.Post("/category", middleware.Auth, controllers.CategoryCreate)
+	route.Put("/category/:id", middleware.Auth, controllers.CategoryUpdate)
+	route.Delete("/category/:id", middleware.Auth, controllers.CategoryDelete)
 
 	// r.Post("/book", utils.HandleSingleFile, controllers.BookCreate)
 
